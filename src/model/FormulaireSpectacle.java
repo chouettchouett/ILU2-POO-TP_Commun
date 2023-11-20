b7561493a8f78c6c3d046496b3c50e856b4dfdd6
chouettchouett <114426961+chouettchouett@users.noreply.github.com>
2023-11-20 21:28:58 +0100

    Update FormulaireSpectacle.java

--- src/model/FormulaireSpectacle.java
+++ src/model/FormulaireSpectacle.java
@@ -1,5 +1,12 @@
 package model;
 
-public class FormulaireSpectacle {
+public class FormulaireSpectacle extends Formulaire {
+
+	int zone;
+
+	public FormulaireSpectacle(int jour, int mois, int zone) {
+		super(jour, mois);
+		this.zone = zone;
+	}
 
 }
